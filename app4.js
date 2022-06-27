79adf8ebfa78fb34c29c357b2a655a7735c80228
pushkar-0553 <105909645+pushkar-0553@users.noreply.github.com>
2022-06-27 19:38:02 +0530

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,129 @@
+// media controllers
+const playPause = document.querySelector("#play-stop");
+const backward = document.querySelector("#backward");
+const forward = document.querySelector("#forward");
+
+// record player animation
+const circleBig = document.querySelector("#circle-bg");
+const circleSm = document.querySelector("#circle-sm");
+
+// playing song
+const songName = document.querySelector("#song-name");
+const audio = document.querySelector("#audio");
+const coverArt = document.querySelector("#cover");
+const musicbox = document.querySelector("#musicbox");
+
+// control button images
+let playImg = "play.svg";
+let pauseImg = "pause.svg";
+
+// default controls
+playPause.src = playImg;
+let isPlaying = true;
+
+const songList = [
+    {
+        name: "The Avengers",
+        source: "The Avengers.mp3",
+        cover: "avengers.jpg"
+    },
+    {
+        name: "Black Panther",
+        source: "black panther.mp3",
+        cover: "black panther.jpg"
+    },
+    {
+        name: "Software developer",
+        source: "The Software Developer.mp3",
+        cover: "soft.jpeg"
+    },
+    {
+        name: "Iron man-3",
+        source: "Iron Man 3.mp3",
+        cover: "iron man.jpg",
+    },
+    {
+        name: "Pink Panther",
+        source: "Pink Panther.mp3",
+        cover: "pink panther.jpg"
+    }
+];
+// helper function
+function createEle(ele) {
+    return document.createElement(ele);
+}
+function append(parent, child) {
+    return parent.append(child);
+}
+// creating track list
+const ul = createEle('ul')
+function createPlayList() {
+    songList.forEach((song) => {
+        let h3 = createEle('h3');
+        let li = createEle('li');
+
+        li.classList.add("track-item");
+        h3.innerText = song.name;
+        append(li,h3);
+        append(ul,li)
+    })
+    append(musicbox, ul);
+}
+
+let songIndex = 0;
+// preloaded song
+loadMusic(songList[songIndex]);
+
+
+function loadMusic() {
+    coverArt.src = songList[songIndex].cover;
+    songName.innerText = songList[songIndex].name;
+    audio.src = songList[songIndex].source;
+}
+
+function playSong() {
+    playPause.src = pauseImg;
+    circleBig.classList.add("animate");
+    circleSm.classList.add("animate");
+
+    audio.play();
+}
+
+function pauseSong() {
+    playPause.src = playImg;
+    circleBig.classList.remove("animate");
+    circleSm.classList.remove("animate");
+
+    audio.pause();
+}
+
+function nextPlay() {
+    songIndex++;
+    if(songIndex > songList.length - 1) {
+        songIndex = 0;
+    }
+    loadMusic(songList[songIndex]);
+    playSong()
+}
+
+function backPlay() {
+    songIndex--;
+    if(songIndex < 0) {
+        songIndex = songList.length - 1;
+    }
+    loadMusic(songList[songIndex]);
+    playSong()
+}
+function playHandler() {
+    isPlaying = !isPlaying;
+    //console.log("Change: ",isPlaying)
+    isPlaying ? pauseSong() : playSong();
+}
+
+
+// player event 
+playPause.addEventListener("click", playHandler);
+backward.addEventListener("click", backPlay);
+forward.addEventListener("click", nextPlay);
+
+createPlayList()
